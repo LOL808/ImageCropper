63620a588073ae211379178f391f3d3798715a36
hushaohao <shhu@gamecdk.com>
2017-06-07 14:34:41 +0800

    no message

---
+++ HSImageCropper/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace HSImageCropper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pathtextBox = new System.Windows.Forms.TextBox();
+            this.pathLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.CoordHint = new System.Windows.Forms.Label();
+            this.saveButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pathtextBox
+            // 
+            this.pathtextBox.Location = new System.Drawing.Point(337, 403);
+            this.pathtextBox.Name = "pathtextBox";
+            this.pathtextBox.ReadOnly = true;
+            this.pathtextBox.Size = new System.Drawing.Size(389, 21);
+            this.pathtextBox.TabIndex = 1;
+            this.pathtextBox.TabStop = false;
+            this.pathtextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // pathLabel
+            // 
+            this.pathLabel.AutoSize = true;
+            this.pathLabel.BackColor = System.Drawing.Color.Transparent;
+            this.pathLabel.Location = new System.Drawing.Point(335, 371);
+            this.pathLabel.Name = "pathLabel";
+            this.pathLabel.Size = new System.Drawing.Size(65, 12);
+            this.pathLabel.TabIndex = 3;
+            this.pathLabel.Text = "资源文件夹";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(651, 430);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "设置路径";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.AutoArrange = false;
+            this.listView1.FullRowSelect = true;
+            this.listView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(325, 54);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.listView1.Size = new System.Drawing.Size(367, 282);
+            this.listView1.TabIndex = 5;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBox.BackgroundImage = global::HSImageCropper.Properties.Resources.logo2;
+            this.pictureBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox.ErrorImage = null;
+            this.pictureBox.InitialImage = null;
+            this.pictureBox.Location = new System.Drawing.Point(68, 54);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(250, 280);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.Click += new System.EventHandler(this.pictureBox_Click);
+            this.pictureBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseDown);
+            this.pictureBox.MouseEnter += new System.EventHandler(this.pictureBox_MouseEnter);
+            this.pictureBox.MouseLeave += new System.EventHandler(this.pictureBox_MouseLeave);
+            this.pictureBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseMove);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.BackColor = System.Drawing.Color.Transparent;
+            this.radioButton1.Location = new System.Drawing.Point(122, 374);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(95, 16);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "裁剪卡牌头像";
+            this.radioButton1.UseVisualStyleBackColor = false;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.BackColor = System.Drawing.Color.Transparent;
+            this.radioButton2.Location = new System.Drawing.Point(122, 408);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(95, 16);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "裁剪小队头像";
+            this.radioButton2.UseVisualStyleBackColor = false;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // CoordHint
+            // 
+            this.CoordHint.AutoSize = true;
+            this.CoordHint.BackColor = System.Drawing.Color.Transparent;
+            this.CoordHint.CausesValidation = false;
+            this.CoordHint.Cursor = System.Windows.Forms.Cursors.No;
+            this.CoordHint.Font = new System.Drawing.Font("宋体", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.CoordHint.Location = new System.Drawing.Point(74, 24);
+            this.CoordHint.Name = "CoordHint";
+            this.CoordHint.Size = new System.Drawing.Size(82, 27);
+            this.CoordHint.TabIndex = 8;
+            this.CoordHint.Text = "(0,0)";
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(604, 12);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(88, 35);
+            this.saveButton.TabIndex = 9;
+            this.saveButton.Text = "保存";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(739, 458);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.CoordHint);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pathLabel);
+            this.Controls.Add(this.pathtextBox);
+            this.Controls.Add(this.pictureBox);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "HSImageCroper";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.TextBox pathtextBox;
+        private System.Windows.Forms.Label pathLabel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label CoordHint;
+        private System.Windows.Forms.Button saveButton;
+    }
+}
+
---
+++ HSImageCropper/Form1.cs
@@ -0,0 +1,388 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace HSImageCropper
+{
+    public partial class Form1 : Form
+    {
+        enum MODE
+        {
+            MODE_AVATAR,
+            MODE_SQUAD,
+            MODE_COUNT
+        }
+
+        enum KeyModifier
+        {
+            None = 0,
+            Alt = 1,
+            Control = 2,
+            Shift = 4,
+            WinKey = 8
+        }
+
+        enum HOTKEYID
+        {
+            HOTKEY_AVATAR = 0,
+            HOTKEY_SQUAD  = 1,
+            HOTKEY_SAVE   = 2,
+            HOTKEY_COUNT
+        }
+
+        class InfoStruct
+        {
+            public InfoStruct()
+            {
+                _name = "";
+                _X = 0;
+                _Y = 0;
+                _squadX = 0;
+                _squadY = 0;
+            }
+            public String _name;
+            public int _X;
+            public int _Y;
+
+            public int _squadX;
+            public int _squadY;
+              
+            public void setAvatarCood(int x, int y)
+            {
+                _X = x;
+                _Y = y;
+            }
+
+            public void setSquadCood(int x, int y)
+            {
+                _squadX = x;
+                _squadY = y;
+            }
+
+            public void setAvatarCood(Point p)
+            {
+                setAvatarCood(p.X, p.Y);
+            }
+
+            public void setSquadCood(Point p)
+            {
+                setSquadCood(p.X, p.Y);
+            }
+
+
+            public String formatCoordinate(bool isSquad = false)
+            {
+                if (isSquad == true)
+                {
+                    return "(" + _squadX + "," + _squadY + ")";
+                }
+                return "(" + _X + "," + _Y + ")";
+            }
+        }
+
+
+        private MODE curMode = MODE.MODE_AVATAR;
+        //private List<String> resImages = new List<string>(100);
+        private String folderPath = "";
+        private List<InfoStruct> resInfo = new List<InfoStruct>(100);
+        private Point cursorPosition;
+
+        private PictureBox hintView = new PictureBox();
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vk);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        public Form1()
+        {
+            InitializeComponent();
+            initListViewHeader();
+            Init();
+        }
+
+        private void registerHotKey()
+        {
+
+         }
+
+        private void Init()
+        {
+            CoordHint.Visible = false;
+            radioButton1.Checked = true;
+            hintView.BackColor = Color.FromArgb(0, 0, 0, 0);
+            //BorderStyle
+            hintView.Enabled = false;
+            
+            hintView.Paint += new System.Windows.Forms.PaintEventHandler(this.hintview_Paint);
+
+            int avatarID = (int)HOTKEYID.HOTKEY_AVATAR;
+            RegisterHotKey(this.Handle, avatarID, (int)KeyModifier.None, Keys.Q.GetHashCode());
+            int squadID = (int)HOTKEYID.HOTKEY_SQUAD;
+            RegisterHotKey(this.Handle, squadID, (int)KeyModifier.None, Keys.W.GetHashCode());
+            int savaID = (int)HOTKEYID.HOTKEY_SAVE;
+            RegisterHotKey(this.Handle, savaID, (int)KeyModifier.Control, Keys.S.GetHashCode());
+
+            pictureBox.Enabled = false;
+
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+
+            if (m.Msg == 0x0312)
+            {
+                /* Note that the three lines below are not needed if you only want to register one hotkey.
+                 * The below lines are useful in case you want to register multiple keys, which you can use a switch with the id as argument, or if you want to know which key/modifier was pressed for some particular reason. */
+
+                Keys key = (Keys)(((int)m.LParam >> 16) & 0xFFFF);                  // The key of the hotkey that was pressed.
+                KeyModifier modifier = (KeyModifier)((int)m.LParam & 0xFFFF);       // The modifier of the hotkey that was pressed.
+                int id = m.WParam.ToInt32();                                        // The id of the hotkey that was pressed.
+
+
+                if (id == (int)HOTKEYID.HOTKEY_AVATAR)
+                {
+                    radioButton1.Checked = true;
+                }
+                else if (id == (int)HOTKEYID.HOTKEY_SQUAD)
+                {
+                    radioButton2.Checked = true;
+                }
+                else if (id == (int)HOTKEYID.HOTKEY_SAVE)
+                {
+                    saveData();
+                }
+            }
+        }
+
+        private void saveData()
+        {
+            string luatable = "local resPosInfo = {\n";
+            foreach(InfoStruct data in resInfo)
+            {
+                string item = String.Format("\t{0}={{avatar={{ x={1},y={2} }},squad={{ x={3},y={4} }}}},\n", System.IO.Path.GetFileNameWithoutExtension(data._name),data._X,data._Y,data._squadX,data._squadY);
+                luatable += item;
+                //Console.WriteLine(item);
+            }
+            luatable += "}\nreturn resPosInfo";
+
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.InitialDirectory = "D:";
+            saveFileDialog1.Filter = "Lua 文件 (*.lua)|*.lua|All files(*.*)|*>**";
+            saveFileDialog1.FilterIndex = 0;
+            saveFileDialog1.RestoreDirectory = true;
+            DialogResult dr = saveFileDialog1.ShowDialog();
+            if (dr == DialogResult.OK && saveFileDialog1.FileName.Length > 0)
+            {
+                FileStream fs = new FileStream(saveFileDialog1.FileName, FileMode.Append);
+                StreamWriter streamWriter = new StreamWriter(fs, Encoding.UTF8);
+                streamWriter.Write(luatable + "\r\n");
+                streamWriter.Flush();
+                streamWriter.Close();
+                fs.Close();
+                //richTextBox1.SaveFile(saveFileDialog1.FileName, RichTextBoxStreamType.PlainText);
+                MessageBox.Show("存储文件成功！");
+            }
+
+            //Console.WriteLine(luatable);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void hintview_Paint(object sender, System.Windows.Forms.PaintEventArgs e)
+        {
+            //Console.WriteLine("Paint");
+            ControlPaint.DrawBorder(e.Graphics, new Rectangle(new Point(0,0),hintView.Size), Color.Ivory, ButtonBorderStyle.Dashed);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            resInfo.Clear();
+            listView1.Items.Clear();
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "请选择文件路径";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                folderPath = dialog.SelectedPath;
+                this.pathtextBox.Text = folderPath;
+                traverseFolder(folderPath);
+            }
+        }
+
+        private void traverseFolder(String path)
+        {
+            DirectoryInfo info = new DirectoryInfo(path);
+            foreach(FileInfo file in info.GetFiles())
+            {
+                String lowwer = file.FullName.ToLower();
+                if (lowwer.EndsWith(".png")||lowwer.EndsWith(".jpg"))
+                {
+                    string fileName = System.IO.Path.GetFileName(file.FullName);
+                    InfoStruct data = new InfoStruct();
+                    data._name = fileName;
+                    resInfo.Add(data);
+                }
+            }
+            initListView();
+            if (resInfo.Count > 0)
+            {
+                listView1.Items[0].Selected = true;
+                listView1.Items[0].Focused = true;
+                pictureBox.Enabled = true;
+            }
+            else
+            {
+                pictureBox.Enabled = false;
+            }
+        }
+
+        private void initListViewHeader()
+        {
+            this.listView1.Columns.Add("文件名", 175, HorizontalAlignment.Left);
+            this.listView1.Columns.Add("头像数据点", 96, HorizontalAlignment.Right);
+            this.listView1.Columns.Add("部队数据点", 96, HorizontalAlignment.Right);
+            listView1.ColumnWidthChanging += (e, sender) =>
+            {
+                ColumnWidthChangingEventArgs arg = (ColumnWidthChangingEventArgs)sender;
+                arg.Cancel = true;
+                arg.NewWidth = listView1.Columns[arg.ColumnIndex].Width;
+            };
+        }
+
+
+
+        
+        private void initListView()
+        {
+
+            this.listView1.BeginUpdate();   //数据更新，UI暂时挂起，直到EndUpdate绘制控件，可以有效避免闪烁并大大提高加载速度
+
+            
+            foreach(var data in resInfo)
+            {
+                ListViewItem lvi = new ListViewItem();
+                lvi.Text = data._name;
+                lvi.SubItems.Add(data.formatCoordinate());
+                lvi.SubItems.Add(data.formatCoordinate(true));
+                this.listView1.Items.Add(lvi);
+            }
+
+            this.listView1.EndUpdate();  //结束数据处理，UI界面一次性绘制。
+        }
+
+        private void loadImage(int idx)
+        {
+
+            String name = this.listView1.Items[idx].SubItems[0].Text;
+            if (name != null && name != "")
+            {
+                try
+                {
+                    Image img = Image.FromFile(Path.Combine(@folderPath, @name));
+                    this.pictureBox.Image = img;
+                }
+                catch(Exception e)
+                {
+                    MessageBox.Show(e.Message, "噢，遭了");
+                }
+
+            }
+            //listView1.Items[idx].
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView1.SelectedIndices != null && listView1.SelectedIndices.Count > 0)
+            {
+                Console.WriteLine(listView1.SelectedItems[0].Index);
+                loadImage(listView1.SelectedItems[0].Index);
+            }
+        }
+
+        private void pictureBox_Click(object sender, EventArgs e)
+        {
+            int index = listView1.FocusedItem.Index;
+            InfoStruct d = resInfo[index];
+
+            if (curMode == MODE.MODE_AVATAR)
+            {
+                d.setAvatarCood(cursorPosition);
+                this.listView1.FocusedItem.SubItems[1].Text = d.formatCoordinate();
+            }
+            else
+            {
+                d.setSquadCood(cursorPosition);
+                this.listView1.FocusedItem.SubItems[2].Text = d.formatCoordinate(true);
+            }
+            resInfo[index] = d;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            curMode = MODE.MODE_AVATAR;
+            if (this.radioButton1.Checked==true)
+            {
+                hintView.Size = new Size(120, 150);
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            curMode = MODE.MODE_SQUAD;
+            if (radioButton2.Checked == true)
+            {
+                hintView.Size = new Size(120, 80);
+            }
+        }
+
+        private void pictureBox_MouseEnter(object sender, EventArgs e)
+        {
+            CoordHint.Visible = true;
+            pictureBox.Controls.Add(hintView);
+        }   
+
+        private void pictureBox_MouseLeave(object sender, EventArgs e)
+        {
+            CoordHint.Visible = false;
+            pictureBox.Controls.Remove(hintView);   
+        }
+
+        private void pictureBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            Point p = e.Location;
+            string X = p.X.ToString();
+            string Y = p.Y.ToString();
+            cursorPosition = p;
+            CoordHint.Text = "(" + X + "," + Y + ")";    
+            hintView.Location = new Point(p.X, p.Y);
+        }
+    
+
+        private void pictureBox_MouseDown(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            if(listView1.Items.Count == 0)
+            {
+                MessageBox.Show("你应该已经醉了", "没数据", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            saveData();
+        }
+    }
+}
